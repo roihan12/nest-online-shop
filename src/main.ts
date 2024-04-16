3675504074e8eaf6045470226edecc89e99bd7d0
Your Name <roihansori12@gmail.com>
2024-04-16 15:46:35 +0700

    fix: bug in prisma seed and jaeger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import init from './tracing';
+import tracing from './tracing';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
@@ -9,7 +9,8 @@ import * as passport from 'passport';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  await init('nest-online-shop');
+  await tracing.start();
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: 'http://localhost:3000',
--- src/tracing.ts
+++ src/tracing.ts
@@ -1,60 +1,58 @@
+'use strict';
+
+// import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { Resource } from '@opentelemetry/resources';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import {
-  ParentBasedSampler,
+  // ConsoleSpanExporter,
   SimpleSpanProcessor,
-  TraceIdRatioBasedSampler,
 } from '@opentelemetry/sdk-trace-base';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import * as process from 'process';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
-import { Resource } from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 
-const init = function (serviceName: string) {
-  console.log('Initializing OpenTelemetry...');
-  const traceExporter = new JaegerExporter({
-    endpoint: 'http://localhost:14268/api/traces',
-  });
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-    }),
-    sampler: new ParentBasedSampler({
-      root: new TraceIdRatioBasedSampler(1),
-    }),
-  });
-
-  provider.addSpanProcessor(new SimpleSpanProcessor(traceExporter));
-  provider.register();
-
-  registerInstrumentations({
-    instrumentations: [
-      new ExpressInstrumentation({
-        requestHook: (span, reqInfo) => {
-          span.setAttribute(
-            'request-headers',
-            JSON.stringify(reqInfo.request.headers),
-          );
-        },
-      }),
-      new HttpInstrumentation(),
-      new NestInstrumentation(),
-    ],
-  });
-  const tracer = provider.getTracer(serviceName);
-  console.log('Success run OpenTelemetry...');
-  // gracefully shut down the SDK on process exit
-  process.on('SIGTERM', () => {
-    provider
-      .shutdown()
-      .then(() => console.log('SDK shut down successfully'))
-      .catch((err) => console.log('Error shutting down SDK', err))
-      .finally(() => process.exit(0));
-  });
-  return { tracer };
+// Configure the SDK to export telemetry data to the console
+// Enable all auto-instrumentations from the meta package
+const exporterOptions = {
+  url: 'http://jaeger:4317', // grcp
+  // url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT +
 };
 
-export default init;
+const traceExporter = new OTLPTraceExporter(exporterOptions);
+
+// const consoleSpanExporter = new ConsoleSpanExporter();
+// const spanProcessor = new SimpleSpanProcessor(consoleSpanExporter);
+
+const spanProcessor = new SimpleSpanProcessor(traceExporter);
+
+const sdk = new NodeSDK({
+  spanProcessor,
+  instrumentations: [
+    // getNodeAutoInstrumentations(),
+    new NestInstrumentation(),
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+  ],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'nest-shop',
+  }),
+});
+
+// initialize the SDK and register with the OpenTelemetry API
+// this enables the API to record telemetry
+sdk.start();
+console.log('Tracing started');
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
+
+export default sdk;
